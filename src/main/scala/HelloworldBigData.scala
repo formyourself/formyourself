a1225269aee4f7a93261c24e62902b50e7930c59
formyourself <contact@formyourself.fr>
2021-03-21 18:12:43 +0530

    initialisation

--- src/main/scala/HelloworldBigData.scala
+++ src/main/scala/HelloworldBigData.scala
@@ -1,8 +1,15 @@
 object HelloworldBigData {
 /* Premier programme Scala */
+  val ma_var_imm : String = "Nacer" /* Variable immutable */
+  private val une_var_imm : String = "Formation Big Data" /* une variable privée est accessible uniquement dans l'objet où elle est déclarée */
+
 def main(args: Array[String]): Unit = {
-  println("Hello World : mon premier programme en Scala")
 
+  var test_mu : Int = 15
+  test_mu = test_mu +10
+  print(test_mu)
+  val test_immu : Int = 20
+  test_immu = 10+2
+  println(test_immu)
 }
-
 }
